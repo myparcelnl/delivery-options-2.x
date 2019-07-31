030e0b48ae89498ba2f8cb87ea72205be510aa3f
Edie Lemoine <edie@myparcel.nl>
2019-07-31 17:06:38 +0200

    MY-12648 comply with the new js sdk interface

--- src/services/fetchFromEndpoint.js
+++ src/services/fetchFromEndpoint.js
@@ -1,7 +1,10 @@
+import { appConfig } from '@/config/appConfig';
 import { configBus } from '@/config/configBus';
 
 export const METHOD_GET = 'get';
 export const METHOD_SEARCH = 'search';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 /**
  * Fetch data from an endpoint and return an object containing the response and errors.
@@ -20,7 +23,10 @@ export const METHOD_SEARCH = 'search';
  */
 export async function fetchFromEndpoint(Endpoint, options = {}, param = {}) {
   const endpoint = new Endpoint();
-  endpoint.config.url = new URL(configBus.config.apiBaseUrl);
+
+  // Set API URL and accept-language header from config
+  endpoint.config.acceptLanguage = configBus.config.locale;
+  endpoint.config.url = new URL(isDev ? appConfig.apiUrl : configBus.config.apiBaseUrl);
 
   let response = {};
   let errors = {};
