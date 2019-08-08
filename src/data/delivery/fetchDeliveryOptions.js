a64c0fd23d1ccb233008500ec64c3cb421ab6228
Edie Lemoine <edie@myparcel.nl>
2019-08-08 15:45:04 +0200

    MY-12648 remove extra checks from fetch functions

--- src/data/delivery/fetchDeliveryOptions.js
+++ src/data/delivery/fetchDeliveryOptions.js
@@ -1,7 +1,5 @@
 import { METHOD_SEARCH, fetchFromEndpoint } from '@/services/fetchFromEndpoint';
 import { Delivery } from 'Sdk/src/endpoint/public/delivery';
-import { ERROR } from '@/config/data/eventConfig';
-import { ERROR_NO_ADDRESS } from '@/config/data/appConfig';
 import { configBus } from '@/config/configBus';
 
 /**
@@ -12,14 +10,6 @@ import { configBus } from '@/config/configBus';
  * @returns {Promise}
  */
 export function fetchDeliveryOptions(carrier = configBus.currentCarrier) {
-  // If the address is not filled in just throw an error immediately.
-  if (!configBus.hasValidAddress) {
-    return new Promise((resolve) => {
-      configBus.$emit(ERROR, { address: ERROR_NO_ADDRESS });
-      resolve({ response: [] });
-    });
-  }
-
   return fetchFromEndpoint(Delivery, {
     method: METHOD_SEARCH,
     params: configBus.getRequestParameters(carrier),
--- src/data/pickup/fetchPickupLocations.js
+++ src/data/pickup/fetchPickupLocations.js
@@ -1,5 +1,4 @@
 import { METHOD_SEARCH, fetchFromEndpoint } from '@/services/fetchFromEndpoint';
-import { ERROR_NO_ADDRESS } from '@/config/data/appConfig';
 import { Pickup } from 'Sdk/src/endpoint/public/pickup';
 import { configBus } from '@/config/configBus';
 
@@ -9,11 +8,6 @@ import { configBus } from '@/config/configBus';
  * @returns {Promise}
  */
 export function fetchPickupLocations() {
-  if (!configBus.hasValidAddress) {
-    configBus.addErrors(Pickup.prototype.endpoint, [ERROR_NO_ADDRESS]);
-    return new Promise((resolve) => resolve({ errors: true, response: {} }));
-  }
-
   return fetchFromEndpoint(Pickup, {
     method: METHOD_SEARCH,
     params: configBus.getRequestParameters(),
