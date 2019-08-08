3cd2083e1992b8e0fef5214c5d93c3cb36b69705
Edie Lemoine <edie@myparcel.nl>
2019-08-08 18:03:30 +0200

    MY-12648 update types

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,26 @@
 declare namespace MyParcel {
-  type Carrier = 'postnl' | 'bpost' | 'dpd';
-  type Platform = 'myparcel' | 'belgie' | 'flespakket';
+  type Carrier = 'postnl' | 'bpost' | 'dpd'
+  type Platform = 'myparcel' | 'belgie' | 'flespakket'
+
+  type DeliveryType = 'morning' | 'standard' | 'evening' | 'pickup' | 'pickup_express'
+  type AdditionalOptionName = 'cooled_delivery' | 'large_format' | 'only_recipient' | 'signature' | 'return'
 
   /**
    * Configuration object supplied by the platform.
    */
   interface CheckoutConfiguration {
-    address: CheckoutAddress,
-    strings: CheckoutStrings,
-    config: CheckoutConfig,
+    address: CheckoutAddress
+    strings: CheckoutStrings
+    config: CheckoutConfig
   }
 
   /**
    * Address object from the external platform.
    */
   interface CheckoutAddress {
-    cc: String,
-    number: String | Number,
-    postalCode: String,
+    cc: String
+    number: String | Number
+    postalCode: String
     city?: String
   }
 
@@ -25,35 +28,43 @@ declare namespace MyParcel {
    * Strings object from the external platform.
    */
   interface CheckoutStrings {
-    city: String,
-    postcode: String,
-    houseNumber: String,
-    addressNotFound: String,
-    again: String,
-    closed: String,
-    discount: String,
-    free: String,
-    from: String,
-    loadMore: String,
-    retry: String,
-    deliveryEveningTitle: String,
-    deliveryMorningTitle: String,
-    deliveryStandardTitle: String,
-    deliveryTitle: String,
-    onlyRecipientTitle: String,
-    pickUpFrom: String,
-    pickupTitle: String,
-    signatureTitle: String,
-    openingHours: String,
+    city: String
+    postcode: String
+    houseNumber: String
+    addressNotFound: String
+    again: String
+    closed: String
+    discount: String
+    free: String
+    from: String
+    loadMore: String
+    retry: String
+    deliveryEveningTitle: String
+    deliveryMorningTitle: String
+    deliveryStandardTitle: String
+    deliveryTitle: String
+    onlyRecipientTitle: String
+    pickUpFrom: String
+    pickupTitle: String
+    signatureTitle: String
+    openingHours: String
 
     // BE only
-    saturdayDeliveryTitle?: String,
-    wrongPostalCodeCity?: String,
+    saturdayDeliveryTitle?: String
+    wrongPostalCodeCity?: String
 
     // NL only
-    beDeliveryStandardTitle?: String,
-    beDeliveryTitle?: String,
-    wrongHouseNumberPostcode?: String,
+    beDeliveryStandardTitle?: String
+    beDeliveryTitle?: String
+    wrongHouseNumberPostcode?: String
+  }
+
+  /**
+   * Response from /delivery_options
+   */
+  interface DeliveryOption {
+    date: Timestamp
+    possibilities: DeliveryPossibility[]
   }
 
   /**
@@ -61,126 +72,134 @@ declare namespace MyParcel {
    */
   interface PickupLocation {
     address: {
-      cc: String,
+      cc: String
       city: String
-      number: String,
-      postal_code: String,
-      street: String,
-    },
+      number: String
+      postal_code: String
+      street: String
+    }
     location: {
-      distance: String,
-      latitude: String,
-      location_code: String,
-      location_name: String,
-      longitude: String,
-      phone_number: String,
-      retail_network_id: String,
+      distance: String
+      latitude: String
+      location_code: String
+      location_name: String
+      longitude: String
+      phone_number: String
+      retail_network_id: String
       opening_hours: {
-        monday: StartEndDate[],
-        tuesday: StartEndDate[],
-        wednesday: StartEndDate[],
-        thursday: StartEndDate[],
-        friday: StartEndDate[],
-        saturday: StartEndDate[],
+        monday: StartEndDate[]
+        tuesday: StartEndDate[]
+        wednesday: StartEndDate[]
+        thursday: StartEndDate[]
+        friday: StartEndDate[]
+        saturday: StartEndDate[]
         sunday: StartEndDate[]
       }
-    },
-    possibilities: DeliveryPossibility[]
+    }
+    possibilities: PickupPossibility[]
   }
 
   /**
    * Configuration object from the external platform.
    */
   interface CheckoutConfig {
-    apiBaseUrl: String,
-    locale: String,
-    carriers: String | Array<String>,
-    platform: Platform,
-    currency: String,
+    apiBaseUrl: String
+    locale: String
+    carriers: String | Array<String>
+    platform: Platform
+    currency: String
 
-    cutoffTime: String,
-    deliveryDaysWindow: String | Number,
-    dropOffDays: String,
-    dropOffDelay: String | Number,
+    cutoffTime: String
+    deliveryDaysWindow: String | Number
+    dropOffDays: String
+    dropOffDelay: String | Number
 
     // BE only
-    saturdayCutoffTime?: String,
+    saturdayCutoffTime?: String
 
-    allowDeliveryOptions: Boolean,
-    allowPickupPoints: Boolean,
+    allowDeliveryOptions: Boolean
+    allowPickupPoints: Boolean
 
-    carrierSettings: CarrierSettings,
+    carrierSettings: CarrierSettings
   }
 
   type CarrierSettings = {
     [key in Carrier]?: {
-      allowEveningDelivery?: Boolean,
-      allowMorningDelivery?: Boolean,
-      allowOnlyRecipient?: Boolean,
-      allowPickupExpress?: Boolean,
-      allowSignature?: Boolean,
+      allowEveningDelivery?: Boolean
+      allowMorningDelivery?: Boolean
+      allowOnlyRecipient?: Boolean
+      allowPickupExpress?: Boolean
+      allowSignature?: Boolean
 
       // NL only
-      allowSaturdayDelivery?: Boolean,
+      allowSaturdayDelivery?: Boolean
 
       // BE only
-      allowMondayDelivery?: Boolean,
+      allowMondayDelivery?: Boolean
 
-      priceEveningDelivery?: Number,
-      priceMorningDelivery?: Number,
-      priceOnlyRecipient?: Number,
-      pricePickup?: Number,
-      pricePickupExpress?: Number,
-      priceSignature?: Number,
-      priceStandardDelivery?: Number,
-    };
-  };
+      priceEveningDelivery?: Number
+      priceMorningDelivery?: Number
+      priceOnlyRecipient?: Number
+      pricePickup?: Number
+      pricePickupExpress?: Number
+      priceSignature?: Number
+      priceStandardDelivery?: Number
+    }
+  }
 
   interface CarrierData {
-    id: Number,
-    name: String,
-    human: String,
+    id: Number
+    name: String
+    human: String
     meta: {
-      logo_png: String,
-      logo_svg: String,
+      logo_png: String
+      logo_svg: String
     }
   }
 
   interface Timestamp {
-    date: String,
-    timezone: String,
-    timezone_type: Number,
+    date: String
+    timezone: String
+    timezone_type: Number
   }
 
   /**
    * A start and end date object.
    */
   interface StartEndDate {
-    start: Timestamp,
-    end: Timestamp,
+    start: Timestamp
+    end: Timestamp
   }
 
   interface ShipmentOption {
-    name: 'cooled_delivery' | 'large_format' | 'only_recipient' | 'signature' | 'return',
+    name: AdditionalOptionName
     schema: {
-      type: String,
+      type: String
       enum: Boolean[]
     }
   }
 
   interface DeliveryTimeFrame {
-    type: String,
+    type: String
     date_time: Timestamp
   }
 
   interface DeliveryPossibility {
-    type: 'morning' | 'standard' | 'evening',
-    shipment_options: ShipmentOption[],
-    collect_date: any,
-    delivery_time_frames: DeliveryTimeFrame[],
+    type: DeliveryType
+    shipment_options: ShipmentOption[]
+    collect_date: any
+    delivery_time_frames: DeliveryTimeFrame[]
+  }
+
+  interface PickupPossibility {
+    delivery_type_id: Number
+    delivery_type_name: DeliveryType
+    moment: {
+      start: Timestamp
+    }
   }
 }
 
 declare module 'MyParcel' {
-  export = MyParcel;
+  export = MyParcel
 }
