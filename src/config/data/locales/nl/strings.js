17ad1cd2a9d95b2b3e45a2f9f83844600624f58c
Edie Lemoine <edie@myparcel.nl>
2019-08-29 14:47:37 +0200

    MY-12648 actually make nl strings dutch :)

--- src/config/data/locales/nl/strings.js
+++ src/config/data/locales/nl/strings.js
@@ -1,7 +1,7 @@
 import * as SETTINGS from '@/config/data/settingsConfig';
 
 export const strings = {
-  [SETTINGS.BE_DELIVERY_STANDARD_TITLE]: 'Standard delivery',
-  [SETTINGS.BE_DELIVERY_TITLE]: 'Delivery',
-  [SETTINGS.WRONG_HOUSE_NUMBER_POSTCODE]: 'House number/postcode combination unknown',
+  [SETTINGS.BE_DELIVERY_STANDARD_TITLE]: 'Standaardlevering',
+  [SETTINGS.BE_DELIVERY_TITLE]: 'Thuis of op het werk bezorgen',
+  [SETTINGS.WRONG_HOUSE_NUMBER_POSTCODE]: 'Huisnummer/postcode combinatie onbekend',
 };
