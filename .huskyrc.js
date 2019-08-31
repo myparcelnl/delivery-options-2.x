6cfddf2e9f7f2423512cf3924f595a9e845b6c99
Edie Lemoine <edie@myparcel.nl>
2019-08-31 19:45:32 +0200

    MY-12648 update husky config and update dist

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,18 @@
+const createCommand = (input) => {
+  return input.join(' && ');
+};
+
+const preCommit = createCommand([
+  'npm run build',
+  'git add dist',
+  'cd myparcel-js-sdk',
+  'npm run build',
+  'cd ..',
+  'git add myparcel-js-sdk',
+]);
+
 module.exports = {
   hooks: {
-    'pre-commit': 'npm run build && git add dist/myparcel.js',
+    'pre-commit': preCommit,
   },
 };
