dbdf23a0d71b9f386e22225914a3b0fec95b81b0
Edie Lemoine <edie@myparcel.nl>
2019-09-19 10:26:58 +0200

    Update default request parameters, adding workaround for dpd bug and don't send parameters if they're undefined

--- src/data/request/getDefaultRequestParameters.js
+++ src/data/request/getDefaultRequestParameters.js
@@ -6,18 +6,43 @@ import { configBus } from '@/config/configBus';
  *
  * @returns {Object}
  */
-export const getDefaultRequestParameters = () => ({
+export const getDefaultRequestParameters = () => {
+  const parameters = {
+    /**
+     * The endpoints we use in this application follow the JSON API "Inclusion of Related Resources" standard.
+     *
+     * @see https://jsonapi.org/format/#fetching-includes
+     */
+    include: 'shipment_options',
+
+    platform: configBus.get(SETTINGS.PLATFORM),
+    carrier: configBus.currentCarrier,
+  };
+
+  const addressValues = {
+    cc: configBus.address.cc,
+    city: configBus.address.city,
+    number: configBus.address.number,
+    postal_code: configBus.address.postalCode,
+  };
+
   /**
-   * The endpoints we use in this application follow the JSON API "Inclusion of Related Resources" standard.
-   *
-   * @see https://jsonapi.org/format/#fetching-includes
+   * Skip undefined entries, if any.
    */
-  include: 'shipment_options',
+  Object.keys(addressValues).forEach((key) => {
+    if (!!addressValues[key]) {
+      /**
+       * TODO: https://jira.dmp.zone/browse/MY-14888.
+       *  This is a temporary workaround for the validation for the dpd endpoint; It gives a 422 response if there are
+       *  location parameters it does not expect, city in this case. Can be removed once above story is fixed.
+       */
+      if (configBus.currentCarrier === 'dpd' && parameters[key] === 'city') {
+        return;
+      }
 
-  platform: configBus.get(SETTINGS.PLATFORM),
-  carrier: configBus.currentCarrier,
+      parameters[key] = addressValues[key];
+    }
+  });
 
-  cc: configBus.address.cc,
-  postal_code: configBus.address.postalCode,
-  number: configBus.address.number,
-});
+  return parameters;
+};
