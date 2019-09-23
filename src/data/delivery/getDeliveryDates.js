33da85bd6274b5fb2b3e99243910946ef0ee652c
Edie Lemoine <edie@myparcel.nl>
2019-09-23 16:56:41 +0200

    allow hiding delivery date through delivery days window

--- src/data/delivery/getDeliveryDates.js
+++ src/data/delivery/getDeliveryDates.js
@@ -1,5 +1,17 @@
-import { LOCALE } from '@/config/data/settingsConfig';
+import * as SETTINGS from '@/config/data/settingsConfig';
 import { configBus } from '@/config/configBus';
+
+/**
+ * Helper function for date.toLocaleDateString with locale from the config bus and default format.
+ *
+ * @param {Date} date - Date to format.
+ * @param {Object} format - The format to apply.
+ *
+ * @returns {string}
+ */
+function toLocaleDateString(date, format = { month: 'numeric', day: 'numeric', year: 'numeric' }) {
+  return date.toLocaleDateString(configBus.get(SETTINGS.LOCALE), format);
+}
 
 /**
  *
@@ -12,21 +24,32 @@ export function getDeliveryDates(deliveryOptions) {
     return [];
   }
 
+  // If the delivery days window is 0, don't show the delivery date to the user. We do this by just passing an empty
+  //  string as the label.
+  if (configBus.get(SETTINGS.DELIVERY_DAYS_WINDOW) === 0) {
+    return [
+      {
+        name: toLocaleDateString(new Date(deliveryOptions[0].date.date)),
+        label: '',
+      },
+    ];
+  }
+
   return deliveryOptions.map(({ date: option }) => {
     const date = new Date(option.date);
-    const name = date.toLocaleDateString(configBus.get(LOCALE), {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric',
-    });
+    const name = toLocaleDateString(date);
 
-    const dateString = date.toLocaleDateString(configBus.get(LOCALE), {
+    const dateString = toLocaleDateString(date, {
       weekday: 'long',
       month: 'long',
       day: 'numeric',
     });
 
     const label = dateString.charAt(0).toUpperCase() + dateString.slice(1);
-    return { label, name };
+
+    return {
+      label,
+      name,
+    };
   });
 }
