9b31ab57a951545d8798176f2152ec337d738bdd
Edie Lemoine <edie@myparcel.nl>
2019-10-21 16:09:48 +0200

    fix: error in getDefaultRequestParameters.js causing the dpd workaround to not work

--- src/data/request/getDefaultRequestParameters.js
+++ src/data/request/getDefaultRequestParameters.js
@@ -26,24 +26,24 @@ export const getDefaultRequestParameters = () => {
     postal_code: configBus.address.postalCode,
   };
 
-  /**
-   * Skip undefined entries, if any.
-   */
   Object.keys(addressValues).forEach((key) => {
-    parameters[key] = addressValues[key];
-
-    if (!!addressValues[key]) {
-      /**
-       * TODO: https://jira.dmp.zone/browse/MY-14888.
-       *  This is a temporary workaround for the validation for the dpd endpoint; It gives a 422 response if there are
-       *  location parameters it does not expect, city in this case. Can be removed once above story is fixed.
-       */
-      if (configBus.currentCarrier === 'dpd' && parameters[key] === 'city') {
-        return;
-      }
-
-      parameters[key] = addressValues[key];
+    /**
+     * Skip undefined items.
+     */
+    if (!addressValues[key]) {
+      return;
     }
+
+    /**
+     * TODO: https://jira.dmp.zone/browse/MY-14888.
+     *  This is a temporary workaround for the validation for the dpd endpoint; It gives a 422 response if there are
+     *  location parameters it does not expect, city in this case. Can be removed once above story is fixed.
+     */
+    if (configBus.currentCarrier === 'dpd' && parameters[key] === 'city') {
+      return;
+    }
+
+    parameters[key] = addressValues[key];
   });
 
   return parameters;
