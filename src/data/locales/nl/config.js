00366793b5eb41d83af8b740c9a72f6a8fc6ddfd
Edie Lemoine <edie@myparcel.nl>
2020-04-16 17:43:47 +0200

    fix(dev): remove unnecessary default apiBaseUrl to fix url being incorrect sometimes

--- src/data/locales/nl/config.js
+++ src/data/locales/nl/config.js
@@ -2,7 +2,6 @@ import * as CARRIERS from '@/data/keys/carrierKeys';
 import * as CONFIG from '@/data/keys/configKeys';
 
 export const config = {
-  [CONFIG.API_BASE_URL]: 'https://api.myparcel.nl',
   [CONFIG.LOCALE]: 'nl-NL',
   [CONFIG.ALLOW_MONDAY_DELIVERY]: true,
   [CONFIG.SATURDAY_CUTOFF_TIME]: '16:00',
