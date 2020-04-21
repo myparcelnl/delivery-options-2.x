7efd342e37696d946b01c8284068684ff524aba2
Edie Lemoine <edie@myparcel.nl>
2020-04-21 11:26:11 +0200

    78096f2

---
+++ precache-manifest.032b39eb653e3dd98185e1d907ed9563.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e98ec241d7d854b11f6dc4f8736e1e",
+    "url": "/delivery-options/index.html"
+  },
+  {
+    "revision": "d060a2d876a699707e0f",
+    "url": "/delivery-options/js/PreLoader.e2541599.js"
+  },
+  {
+    "revision": "664931617e23634a705c",
+    "url": "/delivery-options/js/SandboxFooter.7b764868.js"
+  },
+  {
+    "revision": "a65f8f61a822f4d1dadb",
+    "url": "/delivery-options/js/SandboxHeader.c0bc37be.js"
+  },
+  {
+    "revision": "40e5b2ff7353d986dd10",
+    "url": "/delivery-options/js/SandboxIntroduction.4eef266e.js"
+  },
+  {
+    "revision": "dd472792637f4f05e566",
+    "url": "/delivery-options/js/SandboxSettings.f2ab658e.js"
+  },
+  {
+    "revision": "7aec544ec41349a35a74",
+    "url": "/delivery-options/js/chunk-74556f0a.145309d5.js"
+  },
+  {
+    "revision": "1de77bd503366722964f",
+    "url": "/delivery-options/js/chunk-vendors.0bff695a.js"
+  },
+  {
+    "revision": "acadf9da08afc0b28f14",
+    "url": "/delivery-options/js/components/CCodeDisplay.f6325b97.js"
+  },
+  {
+    "revision": "aa97df55a61574413d194aa44c80af51",
+    "url": "/delivery-options/manifest.json"
+  },
+  {
+    "revision": "f2ae268f7254f42481bd53ad5598bb8b",
+    "url": "/delivery-options/myparcel.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/delivery-options/robots.txt"
+  },
+  {
+    "revision": "802623c2658a8e94e4b6",
+    "url": "/delivery-options/sandbox.js"
+  }
+]);
